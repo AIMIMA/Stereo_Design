52657870723d6364a4f9b3220ab5f063073cfb2e
Abdullah Ibrahim Mohammed <ed21.abdullahibrahimm@alexu.edu.ed>
2023-07-04 23:12:40 +0300

    Original Project

---
+++ main.js
@@ -0,0 +1,27 @@
+let active = document.querySelectorAll(
+  ".wrapper .container .socials .category div i"
+);
+active.forEach(function (ele) {
+  ele.onclick = function () {
+    active.forEach(function (ele) {
+      ele.classList.remove("active");
+      ele.classList.add("text-cyan-500");
+    });
+    this.classList.remove("text-cyan-500");
+    this.classList.add("active");
+  };
+});
+
+// Toggle THeme
+const html = document.querySelector("html");
+const toggleTheme = document.querySelector("#toggle-theme");
+const toggleThemeIcon = document.querySelector("img");
+const changeThemeImg = document.querySelector(".equ");
+toggleTheme.addEventListener("click", () => {
+  html.classList.toggle("dark");
+  const isDark = html.classList.contains("dark");
+  const themeIcon = isDark ? "light" : "dark";
+  const themeImg = isDark ? "light" : "dark";
+  toggleThemeIcon.src = `../img/icon-${themeIcon}.svg`;
+  changeThemeImg.src = `../img/eq-${themeImg}.png`;
+});
